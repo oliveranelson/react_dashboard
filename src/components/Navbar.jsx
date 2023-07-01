70cf80503951df033a6a76cf00a9a622476e1408
oliveranelson <bogicevic.olivera@gmail.com>
2023-07-01 20:50:09 +0200

    Add Orders, Employees & Customers to Pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -89,7 +89,7 @@ const Navbar = () => {
             <p>
               <span className="text-gray-400 text-14">
                 Hi, </span> {" "}
-              <span className="text-gray-400 font-bold ml-1 text-14">Michael</span>
+              <span className="text-gray-400 font-bold ml-1 text-14">Bubba</span>
             </p>
             <MdKeyboardArrowDown
             className="text-gray-400 text-14"/>
